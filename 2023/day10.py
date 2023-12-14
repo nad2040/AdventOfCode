b301dd9c98c6c3df06ab1ec1300f222f679236e2
nad <58147198+nad2040@users.noreply.github.com>
2023-12-14 17:52:35 -0500

    day10p2

--- 2023/day10.py
+++ 2023/day10.py
@@ -5,8 +5,9 @@ pipechars = []
 
 def parse():
     global start,pipechars
-    with open("test10") as f:
+    with open("input10") as f:
         for row, line in enumerate(f):
+            pipechars.append(list(line.strip()))
             for col, char in enumerate(line):
                 match char:
                     case '|':
@@ -26,12 +27,6 @@ def parse():
                     case _:
                         continue
 
-    with open("test10") as f:
-        for line in f:
-            pipechars.append(list(line.strip()))
-
-
-
     start_neighbors = []
     start_row,start_col = start
     for dr,dc in [(1,0),(0,1),(-1,0),(0,-1)]:
@@ -39,9 +34,17 @@ def parse():
         if start in graph.get(neighbor, []):
             start_neighbors.append(neighbor)
     graph[start] = start_neighbors
-    print(graph)
-    print(start)
-    print(graph[start])
+    # print(graph)
+    # print(start)
+    # print(graph[start])
+
+    pipechars[start_row][start_col] = \
+            '|' if (start_row-1,start_col) in graph and (start_row+1,start_col) in graph else \
+            '-' if (start_row,start_col-1) in graph and (start_row,start_col+1) in graph else \
+            '7' if (start_row,start_col-1) in graph and (start_row+1,start_col) in graph else \
+            'J' if (start_row-1,start_col) in graph and (start_row,start_col-1) in graph else \
+            'L' if (start_row-1,start_col) in graph and (start_row,start_col+1) in graph else \
+            'F'
 
     # calc path
     path = set([start])
@@ -66,27 +69,37 @@ def parse():
         pipechars[r][c] = '.'
         del graph[k]
 
-    pipechars = '\n'.join([''.join(c for c in row) for row in pipechars])
-    print(pipechars)
+    # print('\n'.join([''.join(c for c in row) for row in pipechars]))
 
 
 def part1():
     global graph
-
     print(len(graph)//2)
 
 
 def part2():
     global graph
+    count_inside = 0
+    is_inside = False
     row = 0
-    while row < 140:
+    while row < len(pipechars):
         col = 0
-        while col < 140:
-            for
+        while col < len(pipechars[0]):
+            pipe = pipechars[row][col]
+            if pipe == 'F' or pipe == 'L':
+                second_pipe = '.'
+                while (second_pipe := pipechars[row][col]) != '7' and second_pipe != 'J':
+                    col += 1
+                # print(pipe, second_pipe)
+                if pipe == 'F' and second_pipe == 'J' or pipe == 'L' and second_pipe == '7':
+                    is_inside = not is_inside
+            elif pipe == '|':
+                is_inside = not is_inside
+            elif is_inside:
+                count_inside += 1
             col += 1
         row += 1
-
-
+    print(count_inside)
 
 parse()
 part1()
